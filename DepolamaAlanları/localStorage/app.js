648e69b7122f00b5c7ebabac208b7e4491951696
Yılmaz ZEREN <56710444+yilmazzeren@users.noreply.github.com>
2019-11-22 14:50:20 +0300

    Update app.js

--- DepolamaAlanları/localStorage/app.js
+++ DepolamaAlanları/localStorage/app.js
@@ -2,57 +2,58 @@
 
 //SetItem --> değer atama
 
-// localStorage.setItem("hareket","burpee"); // key hareket/  value burpee
+ localStorage.setItem("hareket","burpee"); // key hareket/  value burpee
 
-// localStorage.setItem("tekrar",50);
+ localStorage.setItem("tekrar",50);
 // 50 değeride string olarak kaydedilir.
+
 
 //Get Item --> Key'e göre değer alma
 
-// const value = localStorage.getItem("tekrar");
+ const value = localStorage.getItem("tekrar");
 
-// console.log(typeof value); // string 
+console.log(typeof value); // string 
 
-// // Clear Local Storage 
 
-// localStorage.clear(); // localStorage temizleme.
+// Clear Local Storage 
+
+localStorage.clear(); // localStorage temizleme.
 
 
 // localStorage.setItem("hareket","burpee"); 
 
 // localStorage.setItem("tekrar",50);
 
-// console.log(localStorage.getItem("sport"));// null değer gelir çünkü böyle bir değer girmedik.
+console.log(localStorage.getItem("sport"));// null değer gelir çünkü böyle bir değer girmedik.
 
-// if(localStorage.getItem("hareket")===null){
-//     console.log("sorguladıgınız verı bulunmuyor")
-// }
-// else{
-//     console.log("bulunuyor.")
-// }
+if(localStorage.getItem("hareket")===null){
+     console.log("sorguladıgınız verı bulunmuyor")
+ }
+ else{
+     console.log("bulunuyor.")
+ }
 
 
 
 //Local Storage Array yazma
 
-// const todos = ["todo 1","todo 2", "todo 3"]; // bu sekılde array olarak yazmaz. String olarak yazar.
-// // localStorage.setItem("todolar",todos);
+ const todos = ["todo 1","todo 2", "todo 3"]; // bu sekılde array olarak yazmaz. String olarak yazar.
+ localStorage.setItem("todolar",todos);
 
 
 
-// localStorage.setItem("todolar",JSON.stringify(todos));
-// // JSON ' ın bu özelliği sayesinde biz localstorage'a bır arrya ekleyebiliriz.
+ localStorage.setItem("todolar",JSON.stringify(todos)); // JSON ' ın bu özelliği sayesinde biz localstorage'a bır arrya ekleyebiliriz.
 
 
 
-// const array = localStorage.getItem("todolar"); // Bu şekilde hala string bir değer olarak görmekteyiz.
-// console.log(typeof array);
+const array = localStorage.getItem("todolar"); // Bu şekilde hala string bir değer olarak görmekteyiz.
+console.log(typeof array);
 
 
 
-// const array = JSON.parse(localStorage.getItem("todolar")); 
-// console.log(array);
-// // Bu şekilde artık bir array elde ettik. JSON.parse burda arraya çevirdi.
+const array = JSON.parse(localStorage.getItem("todolar")); 
+console.log(array);
+ // Bu şekilde artık bir array elde ettik. JSON.parse burda arraya çevirdi.
 
 
 
